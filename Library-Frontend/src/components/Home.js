31d2d4feebc86963fee7bfc5347e5f39dc68e6fc
Damian404 <sgt.curren@gmail.com>
2019-09-28 21:48:58 +0100

    Fixed routing options available on home page(admin/user)

--- Library-Frontend/src/components/Home.js
+++ Library-Frontend/src/components/Home.js
@@ -1,46 +1,63 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
-class Home extends Component{
+class Home extends Component {
     constructor() {
         super();
         this.state = {
-            token : localStorage.getItem('authToken')
+            token: localStorage.getItem('authToken')
         }
     }
 
-    componentDidUpdate(){
-        if(this.state.token !== localStorage.getItem('authToken')){
-            this.setState({token : localStorage.getItem('authToken')})
+    componentDidUpdate() {
+        if (this.state.token !== localStorage.getItem('authToken')) {
+            this.setState({ token: localStorage.getItem('authToken') })
         }
     }
 
-    render(){
+    render() {
         return (
             <div>
-            <h1>Home</h1>
-            <div>
-                <Link to="/">Home</Link>
+                <h1>Home</h1>
+                <div>
+                    <Link to="/">Home</Link>
+                </div>
+                {(this.state.token.admin == 1) ? (
+                    <span>
+                        <div>
+                            <Link to="/addBook">Add Book</Link>
+                        </div>
+                        <div>
+                            <Link to="/user">View all users(admin only)</Link>
+                        </div>
+                    </span>
+                ) : (
+                        <span />
+                    )}
+                <div>
+                    <Link to="/book">View all books</Link>
+                </div>
+                <div>
+                    <Link to="/user">View all users(admin only)</Link>
+                </div>
+                <div>
+                    <Link to="/user/profile">View profile</Link>
+                </div>
+                <div>
+                    {(this.state.token == null) ? (
+                        <span>
+                            <div>
+                                <Link to="/login">Login</Link>
+                            </div>
+                            <div>
+                                <Link to="/addUser">Sign up</Link>
+                            </div>
+                        </span>
+                    ) : (
+                            <Link to="/logout">Logout</Link>
+                        )}
+                </div>
             </div>
-            <div>
-                <Link to="/addBook">Add Book(admin or same function as user returns book)</Link>
-            </div>
-            <div>
-                <Link to="/book">View all books</Link>
-            </div>
-            <div>
-                <Link to="/addUser">Add User(sign up)</Link>
-            </div>
-            <div>
-                <Link to="/user">View all users(admin only)</Link>
-            </div>
-            <div>
-                <Link to="/user/profile">View profile</Link>
-            </div>
-            <div>
-                {(this.state.token == null) ? (<Link to="/login">Login</Link>) : (<Link to="/logout">Logout</Link>)}
-            </div>
-        </div>
         )
     }
 }
