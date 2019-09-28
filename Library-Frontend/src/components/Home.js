a380ee200b57d5d9aa32b8370373276271138a2f
Damian404 <sgt.curren@gmail.com>
2019-09-29 00:49:27 +0100

    Redesigned Home.js with a navbar. Added some conditional rendering

--- Library-Frontend/src/components/Home.js
+++ Library-Frontend/src/components/Home.js
@@ -18,45 +18,54 @@ class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Home</h1>
-                <div>
-                    <Link to="/">Home</Link>
-                </div>
-                {(this.state.token.admin == 1) ? (
-                    <span>
-                        <div>
-                            <Link to="/addBook">Add Book</Link>
-                        </div>
-                        <div>
-                            <Link to="/user">View all users(admin only)</Link>
-                        </div>
-                    </span>
-                ) : (
-                        <span />
-                    )}
-                <div>
-                    <Link to="/book">View all books</Link>
-                </div>
-                <div>
-                    <Link to="/user">View all users(admin only)</Link>
-                </div>
-                <div>
-                    <Link to="/user/profile">View profile</Link>
-                </div>
-                <div>
-                    {(this.state.token == null) ? (
-                        <span>
-                            <div>
-                                <Link to="/login">Login</Link>
-                            </div>
-                            <div>
-                                <Link to="/addUser">Sign up</Link>
-                            </div>
-                        </span>
-                    ) : (
-                            <Link to="/logout">Logout</Link>
-                        )}
-                </div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <span className="navbar-brand">Damians Library</span>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul className="navbar-nav">
+                            <li className="nav-item active">
+                                <Link className="nav-link" to="/">Home</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/book">View all books</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/user/profile">View profile</Link>
+                            </li>
+                            {(this.state.token != null) ? (
+                                <li className="nav-item dropdown">
+                                    <a className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        Admin Options
+                                </a>
+                                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                        <Link className="dropdown-item" to="/addBook">Add Book</Link>
+                                        <Link className="dropdown-item" to="/user">View all users</Link>
+                                    </div>
+                                </li>) : (
+                                    <span />
+                                )}
+                            {(this.state.token == null) ? (
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/login">Login</Link>
+                                </li>
+                            ) : (
+                                    <li className="nav-item">
+                                        <Link className="nav-link" to="/logout">Logout</Link>
+                                    </li>
+                                )}
+
+                            {(this.state.token == null) ? (
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/addUser">Sign up</Link>
+                                </li>
+                            ) : (
+                                    <span />
+                                )}
+                        </ul>
+                    </div>
+                </nav>
             </div>
         )
     }
